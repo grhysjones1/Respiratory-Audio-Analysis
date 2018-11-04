b49af8854107c78243994e0fb75e8a9f1a0fa86e
Gareth Jones <>
2018-11-04 17:56:32 +0100

    cleaned mel spectrogram code

--- Clean Stethoscope/02. Mel Spectrograms & Labels.py
+++ Clean Stethoscope/02. Mel Spectrograms & Labels.py
@@ -6,29 +6,15 @@ Created on Mon Oct 29 14:28:49 2018
 @author: garethjones
 """
 
-#%%
-
 ''' IMPORTS & GLOBALS '''
 
-import matplotlib.pyplot as plt
 import librosa
 from librosa.display import specshow
 from librosa.feature import melspectrogram
 import pylab
 import warnings
 warnings.filterwarnings('ignore')
-samprate = 44100
 
-
-#%%
-
-''' MAKE SIGNALS MONO AUDIO '''
-
-# select only one channel of stereo signal, and transpose ready for melspectogram
-signals_mono = [signals[i].T[0] for i in range(len(signals))]
-
-
-#%%
 
 ''' GENERATE MEL SPECTOGRAMS FOR SIGNALS '''
 
@@ -38,52 +24,107 @@ fmax = 500  # top frequency to look at
 n_mels = 55  # number of audio frequency bins
 n_fft = [7000, 9000, 11000]  # width of the fft windows
 
-
 # list of 10 mels, with depth 3
 mel_db_list = []
-
-for i in range(len(signals_mono)):
-    
-    mel = [melspectrogram(
-            signals_mono[i],
-            sr = samprate,
-            hop_length = hop_length,
-            n_fft = j,
-            n_mels = 55,
-            fmin = 125,
-            fmax = 500) for j in n_fft]
-    
+for i in range(len(signals_mono)):    
+    # create 3 mel spectrograms with different fft window size, all other variables the same
+    mel = [melspectrogram(signals_mono[i], sr=samprate, hop_length=hop_length, n_fft=j, n_mels=n_mels, fmin=fmin, fmax=fmax) for j in n_fft]
+    # turn spectrograms into log values
     mel_db = [librosa.power_to_db(mel[k],ref=np.max) for k in range(len(mel))]
-    
+    # re-stack these spectrograms into a single array
     mel_db = np.stack(mel_db,axis=-1)
-    
+    # put all spectrograms into a single list
     mel_db_list.append(mel_db)
 
-
-
-#%%
-# find shape of mels
-print(mel_db_grid[0][2].shape)
-
+print('Shape of mel spectrograms = '+str(mel_db_list[0].shape))
 
 
 #%%
 
-''' JUST USE FOR VISUALISATION, NOT INPUT TO MODEL '''
-''' SAVE MELSPECTOGRAMS TO FILE '''
+''' VISUALISE MEL SPECTROGRAMS FOR ONE SIGNAL '''
 
-for i in range(len(mel_db)):
-    pylab.axis('off') # no axis
-    pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
-    specshow(mel_db[i],y_axis='hz',x_axis='time',sr=samprate)
-    pylab.savefig(filepath+'Spectograms/Signal {}.png'.format(i+1),bbox_inches=None,pad_inches=0,dpi=1000)
-    pylab.close()
+signal_num = 0
+
+plt.figure(figsize=(10,8))
+plt.subplots_adjust(hspace=0.63)
+plt.suptitle('Mel Spectrogram for Signal #{}\nhop_length={}, n_mels={}, fmin={}, fmax={}'.format(signal_num,hop_length,n_mels,fmin,fmax),weight='bold')
+
+for i in range(len(n_fft)):
+    plt.subplot(3,1,i+1)
+    plt.title(' FFT Window Length = {}'.format(n_fft[i]),weight='bold',pad=10)
+    specshow(mel_db_list[signal_num][:,:,i],sr=samprate,x_axis='frames')
+plt.show()
+plt.close()
 
 
 #%%
 
-''' FIND GOOD SPECTOGRAM VALUES '''
+''' REDUCE ANNOTATIONS TO LENGTH OF MEL SPECTROGRAM '''
 
+# set the search window and hop size to same as the melspectrogram
+hop_size = hop_length
+search_window = n_fft[1]
+
+# find index points to look at in annotation signal
+indices = list(np.arange(0,len(anno_gates[0]),hop_length))
+
+# now look across the annotation signal and max if there's a 1 in the window frame
+labels_list = []
+for i in range(len(anno_gates)):
+    labels = []
+    
+    for j in indices:    
+        if ((j - search_window/2) > 0) & ((j + search_window/2) < len(anno_gates[i])):
+            label_window = anno_gates[i][int(j-search_window/2):int(j+search_window/2)]
+            max_label = max(label_window)
+            labels.append(max_label)
+        
+        elif (j - search_window/2) < 0:
+            label_window = anno_gates[i][0:int(j+search_window/2)]
+            max_label = max(label_window)
+            labels.append(max_label)
+        
+        elif (j + search_window/2) > len(anno_gates[i]):
+            label_window = anno_gates[i][int(j-search_window/2):len(anno_gates[i])]
+            max_label = max(label_window)
+            labels.append(max_label)
+    
+    labels_list.append(labels)
+    
+    
+#%%
+    
+''' VISUALISE BINARY SIGNAL AND MEL SIZED ANNOTATIONS '''
+
+signal_num = 1
+
+fig,axs = plt.subplots(2,1,figsize=(12,8))
+plt.subplots_adjust(hspace=0.4)
+plt.suptitle('Updated Labels for Mel Spectrograms Signal #{}'.format(signal_num),weight='bold')
+
+axs[0].plot(anno_gates[signal_num])
+axs[0].set_title('Original Binary Annotation Signal',pad=10)
+axs[0].set_xlim(xmin=0,xmax=len(anno_gates[signal_num]))
+axs[0].set_ylim(ymin=0)
+axs[0].spines['top'].set_color('none')
+axs[0].spines['right'].set_color('none')
+
+axs[1].plot(labels_list[signal_num])
+axs[1].set_title('New Annotations at Mel Spectrogram Frame Size',pad=10)
+axs[1].set_xlim(xmin=0,xmax=len(labels_list[signal_num]))
+axs[1].set_ylim(ymin=0)
+axs[1].spines['top'].set_color('none')
+axs[1].spines['right'].set_color('none')
+
+plt.show()
+plt.close()
+
+
+#%%
+
+''' SAVE SPECTROGRAMS TO FILE TO SEE DIFFERENCES '''
+
+'''
 # 1s = 44,100    0.5s = 22,050   0.25s = 11,025   0.125s = 5513    0.0625s = 2756
 
 #hop_frames = [1250,2550,5500,11000,22050]
@@ -107,102 +148,4 @@ for i in fft_width:
         plt.savefig(filepath+'Comparison FFTWindow {} Freqbins {}.png'.format(i,j),dpi=900)
         plt.show()
         plt.close()
-
-
-#%%
-plt.figure(figsize=(15,15))
-for k in range(1,9):
-    meltest = melspectrogram(signal,samprate,hop_length=hop_frames[k-1],n_fft=11025,fmin=fmin,fmax=fmax,n_mels=40)
-    meltest_db = librosa.power_to_db(meltest,ref=np.max)
-    plt.subplot(5,1,k)
-    plt.title('Hop Frames = {}'.format(hop_frames[k-1]))
-    specshow(meltest_db,sr=samprate)
-plt.suptitle('FFT Window Width = 11025 / Number Frequency Bin = 40'.format(i,j))
-plt.savefig(filepath+'Comparison FFTWindow 11025 Freqbins 40.png'.format(i,j),dpi=900)
-plt.show()
-plt.close()
-
-  
-#%%
-
-plt.figure(figsize=(17,25))
-
-meltest = melspectrogram(signal,samprate,hop_length=2550,n_fft=8000,fmin=fmin,fmax=fmax,n_mels=30)
-meltest_db = librosa.power_to_db(meltest,ref=np.max)
-plt.subplot(8,1,1)
-plt.title('FFT 8000 Freqs 30 Hop 2550 ')
-specshow(meltest_db,sr=samprate)
-
-meltest = melspectrogram(signal,samprate,hop_length=2550,n_fft=8000,fmin=fmin,fmax=fmax,n_mels=35)
-meltest_db = librosa.power_to_db(meltest,ref=np.max)
-plt.subplot(8,1,2)
-plt.title('FFT 8000 Freqs 35 Hop 2550 ')
-specshow(meltest_db,sr=samprate)
-
-meltest = melspectrogram(signal,samprate,hop_length=2550,n_fft=8000,fmin=fmin,fmax=fmax,n_mels=40)
-meltest_db = librosa.power_to_db(meltest,ref=np.max)
-plt.subplot(8,1,3)
-plt.title('FFT 8000 Freqs 40 Hop 2550 ')
-specshow(meltest_db,sr=samprate)
-
-meltest = melspectrogram(signal,samprate,hop_length=2550,n_fft=8000,fmin=fmin,fmax=fmax,n_mels=45)
-meltest_db = librosa.power_to_db(meltest,ref=np.max)
-plt.subplot(8,1,4)
-plt.title('FFT 8000 Freqs 45 Hop 2550 ')
-specshow(meltest_db,sr=samprate)
-
-meltest = melspectrogram(signal,samprate,hop_length=1250,n_fft=11025,fmin=fmin,fmax=fmax,n_mels=35)
-meltest_db = librosa.power_to_db(meltest,ref=np.max)
-plt.subplot(8,1,5)
-plt.title('FFT 11025 Freqs 35 Hop 1250 ')
-specshow(meltest_db,sr=samprate)
-
-meltest = melspectrogram(signal,samprate,hop_length=1250,n_fft=11025,fmin=fmin,fmax=fmax,n_mels=40)
-meltest_db = librosa.power_to_db(meltest,ref=np.max)
-plt.subplot(8,1,6)
-plt.title('FFT 11025 Freqs 40 Hop 1250 ')
-specshow(meltest_db,sr=samprate)
-
-meltest = melspectrogram(signal,samprate,hop_length=2550,n_fft=11025,fmin=fmin,fmax=fmax,n_mels=45)
-meltest_db = librosa.power_to_db(meltest,ref=np.max)
-plt.subplot(8,1,7)
-plt.title('FFT 11025 Freqs 45 Hop 2550 ')
-specshow(meltest_db,sr=samprate)
-
-meltest = melspectrogram(signal,samprate,hop_length=5500,n_fft=14000,fmin=fmin,fmax=fmax,n_mels=40)
-meltest_db = librosa.power_to_db(meltest,ref=np.max)
-plt.subplot(8,1,8)
-plt.title('FFT 14000 Freqs 40 Hop 5500 ')
-specshow(meltest_db,sr=samprate)
-
-plt.show()
-plt.close()
-
-
-#%%
-
-''' LOOK AT MELS OF 3 DIFFERENT FFT WINDOWS, SAME HOP AND NMELS '''
-
-fmin = 125
-fmax = 500
-signalnum = 0
-
-plt.figure(figsize=(10,15))
-
-meltest = melspectrogram(signals_mono[signalnum],samprate,hop_length=882,n_fft=5000,fmin=fmin,fmax=fmax,n_mels=55)
-meltest_db = librosa.power_to_db(meltest,ref=np.max)
-plt.subplot(3,1,1)
-plt.title('FFT 8000 Freqs 45 Hop 2550')
-specshow(meltest_db,sr=samprate,x_axis='frames')
-
-meltest = melspectrogram(signals_mono[signalnum],samprate,hop_length=882,n_fft=7000,fmin=fmin,fmax=fmax,n_mels=55)
-meltest_db = librosa.power_to_db(meltest,ref=np.max)
-plt.subplot(3,1,2)
-plt.title('FFT 11025 Freqs 45 Hop 2550')
-specshow(meltest_db,sr=samprate)
-
-meltest = melspectrogram(signals_mono[signalnum],samprate,hop_length=882,n_fft=9000,fmin=fmin,fmax=fmax,n_mels=55)
-meltest_db = librosa.power_to_db(meltest,ref=np.max)
-plt.subplot(3,1,3)
-plt.title('FFT 14000 Freqs 45 Hop 2550')
-specshow(meltest_db,sr=samprate)
+'''
